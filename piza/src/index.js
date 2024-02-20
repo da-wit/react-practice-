0d574cbd8e101e4a416c638f641820d774e89289
Dawit Belay <dawitbelay0707@gmail.com>
2024-02-20 11:29:38 +0300

    initial commit

---
+++ piza/src/index.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { pizzaData } from './data';
+
+
+
+function App(){
+  return <div className='main'>
+    <Header/>
+    <Menu/>
+    <Footer/>
+  </div>
+}
+
+function Header(){
+  return <h1 className='head'>Fast React Pizza co.</h1>
+}
+
+function Menu(){
+  const piza =pizzaData;
+  const numpiza = piza.length;
+
+  return <div className='menu'>
+    <h2 className='menu-h2'>Our menu</h2>
+    <>
+    {/* <p>welcome to our first react pizza app to order food </p> */}
+    {numpiza > 0 ? (<ul className='piza'>
+      {/* {pizzaData.map((pizza) => <Pizza name={pizza.name} image={pizza.photoName} price = {pizza.price}  />)
+      We can also do it in this way}  */}
+      {pizzaData.map(pizza =>
+        <Pizza pizzaObj ={pizza} key={pizza.name} />)}
+   
+    
+    </ul>) : <p>We are still woking on our menu please come back letter </p>}
+
+    </>
+  </div>
+   
+}
+
+function Footer(){
+  const hour = new Date().getHours();
+  const isOpen = 7;
+  const isClosed = 22;
+  // if (isOpen <= hour && hour <= isClosed){   
+  //   return <footer className='footer'> We are currently Open</footer>
+  // };  we can do it this way 
+  const open = isOpen <= hour && hour <= isClosed;
+
+  return <footer className='footer'>{open ? <Order openHour = {isOpen}
+  closeHour = {isClosed}/> : (<p>We are happy to welcome you between {isOpen}:00 and {isClosed}:00 </p>)} 
+     </footer>
+
+
+  // const time = new Date().toLocaleTimeString()
+  // return <footer className='footer'>{time} We are currently Open</footer>
+
+}
+
+function Order({closeHour}){
+  return (<div>
+    <p> 
+    we are open unitl {closeHour}:00, Come vist us or Order us online!
+     </p>
+     <button className='btn'>Order</button>
+  </div>)
+
+}
+
+function Pizza({pizzaObj}){
+  // if (props.pizzaObj. soldOut) return null;
+  return  <li className={`pizza${pizzaObj.soldOut ? "sold-out": ""}`}>
+    {/* <img src={props.image} alt={props.name}/>
+    <div className='pizza-1'>
+    <h1>{props.name}</h1>
+    <p>{props.ingerdinat}</p>
+    <span>{props.price + 4}</span> */}
+
+      <img src={pizzaObj.photoName} alt={pizzaObj.name}/>
+      <div className='pizza-1'>
+        <h1>{pizzaObj.name}</h1>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+    </div>
+  </li>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<React.StrictMode>
+  <App/>
+  
+</React.StrictMode>)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
---
+++ steps/src/App.js
@@ -0,0 +1,93 @@
+import {useState } from 'react'
+const message = [
+  'Learn React js',
+  'Applay for jops',
+  'Invest your new income'
+]
+
+
+export default function App(){
+  
+  const [step,setStep] = useState(1);
+
+  // const [visibility, setDisplay] = useState("block");
+  const [visibility, setDisplay] = useState(true);
+
+  function handlePrevious(){
+    return step > 1? setStep(step -1 ): null;
+  }
+
+  function handleNext(){
+    return step < message.length? setStep(step + 1 ) : null;
+  }
+  
+
+
+  
+  // function handleDisplay(){
+  //   return visibility === true? setDisplay(false): setDisplay(true)
+
+  // }
+
+  return <>
+  <h1 onClick={()=>setDisplay(!visibility)}> &#9830;</h1>
+  { visibility &&
+    ( <div className="steps"  >
+   
+    <div className="numbers">
+      <div className={`${step >=1 ? "active": null } `}>1</div>
+      <div className={`${step>=2 ? "active": null } `}>2</div>
+      <div className={`${step>=3 ? "active": null } `}>3</div>
+    </div>
+
+    <p className="message">Step {step}: {`${message[step -1]}`}</p>
+
+    <div className="buttons">
+        <button style={{backgroundColor:"purple", color:'white' }} onClick={handlePrevious}>Previous</button>
+        <button style={{backgroundColor:"purple", color:'white' }} onClick={handleNext}>Next</button>
+        
+    </div>
+    
+
+  </div>)
+  }
+  <Steps/>
+  </>
+}
+
+ function Steps(){
+  const [steps,setSteps] = useState(0)
+  function handleUpStep(){
+    return setSteps(steps + 1)
+  }
+  function handleDownStep(){
+    return setSteps(steps -1 )
+  }
+
+  const [count,setCount] = useState(steps)
+  function handleUpCount(){
+    return setCount(count + steps)
+  }
+  function handleDownCount(){
+    return setCount(count - steps  )
+  }
+
+  const date = new Date("jun 21 2027")
+  date.setDate(date.getDate() + count)
+
+  return <>
+  <div className='ste'>
+    <button onClick={handleDownStep}>-</button>
+    <p>Step: {steps}</p>
+    <button onClick={handleUpStep}>+</button>
+  </div>
+
+  <div className='ste'>
+    <button onClick={handleDownCount}>-</button>
+    <p>Count:{count}</p>
+    <button onClick={handleUpCount}>+</button>
+  </div>
+  <p>{count === 0? "Today is ": count > 0 ? `${count} Days away from today`: `${Math.abs(count)} Days ago was`}{date.toDateString()}</p>
+   </>
+}
+
