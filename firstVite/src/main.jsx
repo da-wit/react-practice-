f6077372837e35c336d351a7c7d34ef3be15bc4a
Dawit Belay <dawitbelay0707@gmail.com>
2024-03-03 14:45:32 +0300

    final

--- firstVite/src/main.jsx
+++ firstVite/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Quiz from './quiz.jsx'
 import './index.css'
+import Challenge from './Challenge.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Quiz/>
+    <Challenge/>
   </React.StrictMode>,
 )
